708a0398f7fb34eb5422c5c81930f05b5c642322
AlecMGo <AlecMGo@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-01-25 19:47:27 +0000

    Adding .sh scripts that will make testing easier.

---
+++ pa1/run3.sh
@@ -0,0 +1,14 @@
+echo "./rpplan -text -file world1.config -seed 10 -points 26"
+./rpplan -text -file world1.config -seed 10 -points 26
+
+echo
+echo "./rpplan -text -file world1.config -seed 10 -points 49"
+./rpplan -text -file world1.config -seed 10 -points 49
+
+echo
+echo "./rpplan -text -file world2.config -seed 10 -points 49"
+./rpplan -text -file world2.config -seed 10 -points 49
+
+echo
+echo "./rpplan -text -file world2.config -seed 10 -points 60"
+./rpplan -text -file world2.config -seed 10 -points 60
