ae2a8bb5876bfff9c504bd07f27e07a2701fea42
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-02 01:14:53 +0000

    skeleton for haar features

---
+++ trunk/project/code/HaarFeatures.cpp
@@ -0,0 +1,13 @@
+#include "HaarFeatures.h"
+
+HaarFeatures::HaarFeatures() {
+  // potentially reading from haarfeatures.txt here.
+}
+
+HaarFeatures::~HaarFeatures() {}
+
+void HaarFeatures::getFeatureValues(vector<double>& feature_values,
+                        const IplImage* img) const
+{
+}
+
