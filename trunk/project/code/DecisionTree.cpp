113f6d6fc22053e9618fba6633355a386e9e4d96
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-02 01:36:06 +0000

    skeleton for decision trees. generic for any example, need not be images.

---
+++ trunk/project/code/DecisionTree.cpp
@@ -0,0 +1,11 @@
+#include "DecisionTree.h"
+
+void DecisionTree::trainDecisionTree(const vector<TrainingExample>& examples) const {
+  // TODO
+}
+
+bool DecisionTree::predictClassLabel(const Example& example) const {
+  // TODO, returning false for now
+  return false;
+}
+
---
+++ trunk/project/code/DecisionTree.h
@@ -0,0 +1,44 @@
+// Author: Cognitive-Dissonance team
+
+#pragma once
+#include <vector>
+
+using namespace std;
+
+// an example is just a bunch of features... (could be a test or training
+// example). generic enough to just call 'data', but might have name clashes 
+// so using 'Example' for now (but applies to test data as well)...
+class Example {
+public:
+  Example() {}
+  virtual ~Example() {}
+
+  virtual int getNumberOfFeatures() const = 0;
+
+  // features could be any type, so if adding int/string features
+  // provide other functions, for now just double... 
+  virtual double getFeatureDoubleValue(int feature_index) const = 0;
+};
+
+// training example is an example for which we know the class label...
+class TrainingExample : public Example {
+public:
+  TrainingExample() {}
+  virtual ~TrainingExample() {}
+
+  virtual bool getClassLabel() const = 0; // 0/1 for now
+};
+
+// Decision tree operates on generic TrainingExamples and predicts
+// classes for Examples (which could also be TrainingExamples)...
+class DecisionTree {
+public:
+  DecisionTree() {}
+  ~DecisionTree() {}
+
+  void trainDecisionTree(const vector<TrainingExample>& examples) const;
+  bool predictClassLabel(const Example& example) const;
+private:
+
+};
+
