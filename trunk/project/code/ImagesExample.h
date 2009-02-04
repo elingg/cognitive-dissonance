7529244d2f5de1736545f3648516b63859d4138a
elingg <elingg@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-04 07:36:16 +0000

    ImagesExample.h added

---
+++ trunk/project/code/ImagesExample.h
@@ -0,0 +1,26 @@
+#include "DecisionTree.h"
+
+using namespace std;
+
+
+#include 
+class ImagesExample : public TrainingExample {
+public:
+  ImagesExample(
+          vector<double> hfeatures,
+           bool classlabel);
+
+  size_t getNumberOfFeatures() const {
+            return m_featureValues.size();
+          }
+  bool   getClassLabel() const {
+            return m_classLabel;
+          }
+  double getFeatureDoubleValue(int feature_index) const {
+            return m_featureValues[feature_index];
+          }
+private:
+  vector<double> m_featureValues;
+  bool m_classLabel;
+};
+
