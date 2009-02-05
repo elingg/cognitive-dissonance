4f935350a9406f5e1d98bc131d567bf42a9bf391
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-05 03:14:16 +0000

    deleting, so i can replace the chmod flag on this.

--- trunk/project/code/milestone.sh
+++
@@ -1,8 +0,0 @@
-#Milestone - shortcut for running train.
-./train -c config /afs/ir/class/cs221/vision/data/
-#Milestone - shortcut script for running test
-#This is copied from pg. 7 of the handout
-./test -c config -g /afs/ir/class/cs221/vision/data/easy.xml -o test.xml /afs/ir/class/cs221/vision/data/easy.avi
-#Milestone - shortcut script for evaluate
-#This is copied from pg. 9 of the handout
-./evaluate -m test.xml -g /afs/ir/class/cs221/vision/data/easy.xml
