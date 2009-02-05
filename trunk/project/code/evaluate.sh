32f15301f859786a182156ae28514e4b659044d3
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-05 03:10:52 +0000

    scripts for evaluating. script called 'milestone.sh' for train, test and evaluate.

---
+++ trunk/project/code/evaluate.sh
@@ -0,0 +1,3 @@
+#Milestone - shortcut script for evaluate
+#This is copied from pg. 9 of the handout
+./evaluate -m test.xml -g /afs/ir/class/cs221/vision/data/easy.xml
--- trunk/project/code/test.sh
+++ trunk/project/code/test.sh
@@ -1,3 +1,3 @@
 #Milestone - shortcut script for running test
 #This is copied from pg. 7 of the handout
-./test -c config -g /afs/ir/class/cs221/vision/data/easy.xml /afs/ir/class/cs221/vision/data/easy.avi
+./test -c config -g /afs/ir/class/cs221/vision/data/easy.xml -o test.xml /afs/ir/class/cs221/vision/data/easy.avi
