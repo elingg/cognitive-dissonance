64fa270196eaba5bd5a19e89ebea4f590199c129
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-08 21:21:11 +0000

    accuracy for decisiontree.

--- pa2/OneAgainstMany.c
+++ pa2/OneAgainstMany.c
@@ -11,13 +11,37 @@
 
 float DecisionTreeAccuracy(DecisionTreeSet* classifiers,
 			   DigitSet* testSet) {
-  // TODO
-  assert(false); // remove
+  // exactly similar implementation as bagged classifiers...
 
+  const int NUM_DIGITS = 10;
+  int numCorrect = 0;
+  int totalTested = 0;
 
+  //Iterate through all the examples in th test set
+  for(int digitIndex = 0; digitIndex < testSet->numDigits; digitIndex++) {
+    int bestDigit = 0;
+    float maxConfidence = 0.0;
+    
+    //Out of all the digits (from 1 to 10), we find the best one
+    for(int i = 0; i < NUM_DIGITS; i++) {
+      float confidence = PositiveConfidence(classifiers->trees[i], 
+                                            testSet->digits[digitIndex]);
+      if(confidence > maxConfidence) {
+        maxConfidence = confidence;
+        bestDigit = i;
+      }
+    }
 
+    //Check if our choice was the correct one
+    if((testSet->digits[digitIndex]->label) == bestDigit) {
+      numCorrect++;
+    }
+    totalTested++;
+  }
 
-  return 0;
+  return (float) numCorrect / (float) totalTested;
+  // DONETODO
+  // assert(false); // remove
 }
 
 float BaggedDecisionTreeAccuracy(BaggedDecisionTree** classifiers,
