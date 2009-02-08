2659cfdcff8748eacb8bb20a3c202f0abe2a60eb
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-08 21:26:48 +0000

    task1.

---
+++ trunk/pa2/task1.sh
@@ -0,0 +1,14 @@
+./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 4 td_4_1k.out 
+./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 6 td_6_1k.out 
+./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 8 td_8_1k.out 
+./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 10 td_10_1k.out 
+./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 12 td_12_1k.out 
+./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 14 td_14_1k.out 
+
+./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 4 td_4_10k.out 
+./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 6 td_6_10k.out 
+./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 8 td_8_10k.out 
+./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 10 td_10_10k.out 
+./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 12 td_12_10k.out 
+./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 14 td_14_10k.out 
+
