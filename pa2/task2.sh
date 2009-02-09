a42c92ea2a0fe6990a4329468781c1bc6195b74a
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-09 01:13:38 +0000

    tweaking so it'll work from my mac. also duplicate 'in' may have caused this not to work before.

--- pa2/task2.sh
+++ pa2/task2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #Script for Part 2 (bagging)
-DIR=/usr/class/cs221/data/digits
+DIR=/afs/ir/class/cs221/data/digits
 NUM_BAGS=100
 
-for depth in in {3..14}
+for depth in {3..14}
 do
   echo "Running depth: $depth"
   ./digit bagging $DIR/training-1k-images.idx3 $DIR/training-1k-labels.idx1 $DIR/test-1k-images.idx3 $DIR/test-1k-labels.idx1 $depth $NUM_BAGS baggingDepth$depth.out
