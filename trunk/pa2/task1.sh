7b38109cb956f46215013de4f913755d22af277a
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-02-09 00:55:25 +0000

    for loops, stats collections.

--- trunk/pa2/task1.sh
+++ trunk/pa2/task1.sh
@@ -1,14 +1,52 @@
-./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 4 td_4_1k.out 
-./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 6 td_6_1k.out 
-./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 8 td_8_1k.out 
-./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 10 td_10_1k.out 
-./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 12 td_12_1k.out 
-./digit singletree training-1k-images.idx3 training-1k-labels.idx1 test-1k-images.idx3 test-1k-labels.idx1 14 td_14_1k.out 
+#!/bin/bash
 
-./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 4 td_4_10k.out 
-./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 6 td_6_10k.out 
-./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 8 td_8_10k.out 
-./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 10 td_10_10k.out 
-./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 12 td_12_10k.out 
-./digit singletree training-10k-images.idx3 training-10k-labels.idx1 test-10k-images.idx3 test-10k-labels.idx1 14 td_14_10k.out 
+#Script for Part 1 
+DIR=/afs/ir/class/cs221/data/digits
 
+datasize="1k"
+startdepth=4
+stopdepth=14
+stepdepth=2
+
+for ((depth=$startdepth; depth<=$stopdepth; depth=depth+$stepdepth))
+do
+  echo "Depth: $depth, Trained on $datasize"
+  cmd="./digit singletree $DIR/training-$datasize-images.idx3 $DIR/training-$datasize-labels.idx1 $DIR/test-1k-images.idx3 $DIR/test-1k-labels.idx1 $depth singleTree$datasize$depth.out"
+  echo $cmd
+  $cmd
+done
+echo "Accuracies for 1k:"
+echo "On train data:"
+for ((depth=$startdepth; depth<=$stopdepth; depth=depth+$stepdepth))
+do
+  echo "Depth: $depth"
+  cat results.singletree.training.d$depth.txt
+done
+echo "On test data:"
+for ((depth=$startdepth; depth<=$stopdepth; depth=depth+$stepdepth))
+do
+  echo "Depth: $depth"
+  cat results.singletree.test.d$depth.txt
+done
+
+datasize="10k"
+for ((depth=$startdepth; depth<=$stopdepth; depth=depth+$stepdepth))
+do
+  echo "Depth: $depth, Trained on $datasize"
+  cmd="./digit singletree $DIR/training-$datasize-images.idx3 $DIR/training-$datasize-labels.idx1 $DIR/test-1k-images.idx3 $DIR/test-1k-labels.idx1 $depth singleTree$datasize$depth.out"
+  echo $cmd
+  $cmd
+done
+echo "Accuracies for 10k:"
+echo "On train data:"
+for ((depth=$startdepth; depth<=$stopdepth; depth=depth+$stepdepth))
+do
+  echo "Depth: $depth"
+  cat results.singletree.training.d$depth.txt
+done
+echo "On test data:"
+for ((depth=$startdepth; depth<=$stopdepth; depth=depth+$stepdepth))
+do
+  echo "Depth: $depth"
+  cat results.singletree.test.d$depth.txt
+done
