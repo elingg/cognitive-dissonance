432067f6735ba6f3f70f4149c443cc9afbf7bc40
elingg <elingg@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-07 23:28:12 +0000

    updated code for sobel features with histogram- so far not actually using these features in the classifier

--- project/code/EdgeDetectionFeatures.h
+++ project/code/EdgeDetectionFeatures.h
@@ -11,7 +11,7 @@ public:
   EdgeDetectionFeatures();
   ~EdgeDetectionFeatures();
 
-  void getFeatureValues(vector<cvHistogram>& feature_values,
+  void getFeatureValues(vector<CvHistogram>& feature_values,
                         const IplImage* img) const;
 
 private:
