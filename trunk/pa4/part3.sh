357e9571b63b178b23aaf961980a6c5f095cd9f0
AlecMGo <AlecMGo@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-09 06:18:43 +0000

    Part 3 now passes.  Forgot the kidnapped flag

--- trunk/pa4/part3.sh
+++ trunk/pa4/part3.sh
@@ -1,2 +1,2 @@
-./localize -i -e gates1.ppm robot1.log lasers simple > 3-lasers-simple-kidnapped.out
+./localize -i -e gates1.ppm robot1.log lasers simple kidnapped > 3-lasers-simple-kidnapped.out
 diff -q 3-lasers-simple-kidnapped.out sample_outputs/3-lasers-simple-kidnapped.out
