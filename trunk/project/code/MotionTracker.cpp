9e0e53f8e9959fa90db383fb18770640182d3456
AlecMGo <AlecMGo@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-14 03:46:34 +0000

    Externalized KalmanFilter

--- trunk/project/code/MotionTracker.cpp
+++ trunk/project/code/MotionTracker.cpp
@@ -1,5 +1,58 @@
 #include "MotionTracker.h"
+#include "cv.h"
+#include "cxcore.h"
+#include "cvaux.h"
+#include "objects.h"
+#include <iostream>
 
+using namespace std;
+/**
+ * Contains all the motion tracking code
+ */
 MotionTracker::MotionTracker() {}
 MotionTracker::~MotionTracker() {}
 
+KalmanFilter::KalmanFilter() {
+  predictor1 = cvCreateModuleBlobTrackPredictKalman();
+
+  //Hacky way to know if struct is initialized
+  //TODO: Ask Anand how to avoid this hack
+  blob1.x = -1;
+}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::update(CObjectList* classifierObjects) {
+  cout << "Objects found: " << classifierObjects->size() << endl;
+  for(size_t i = 0; i < classifierObjects->size(); i++) {
+    CvBlob blob; 
+    blob.x = classifierObjects->at(i).rect.x;
+    blob.y = classifierObjects->at(i).rect.y;
+    blob.w = classifierObjects->at(i).rect.width;
+    blob.h = classifierObjects->at(i).rect.height;
+    if(blob1.x == -1) {
+      //Assign first blob
+      blob1 = blob;
+      predictor1->Update(&blob);
+    }
+    if(sqrt(pow(blob.x - blob1.x, 2) + pow(blob.y - blob1.y, 2)) < 25) {
+      //Assign first blob
+      blob1 = blob;
+      predictor1->Update(&blob);
+    }
+  }
+  CvBlob* predicted = predictor1->Predict();
+
+  //Add prediction to list of Kalman objects
+  CObject obj;
+  obj.rect = cvRect(0, 0, predicted->w, predicted->h); 
+  obj.rect.x = predicted->x;
+  obj.rect.y = predicted->y;
+  obj.label = string("kalman");
+  kalmanObjects.clear();
+  kalmanObjects.push_back(obj);
+}
+
+CObjectList KalmanFilter::predict() {
+  return kalmanObjects;
+}
--- trunk/project/code/MotionTracker.h
+++ trunk/project/code/MotionTracker.h
@@ -1,4 +1,9 @@
 #pragma once
+#include "cv.h"
+#include "cxcore.h"
+#include "cvaux.h"
+#include "objects.h"
+
 
 class MotionTracker {
   public:
@@ -6,3 +11,17 @@ class MotionTracker {
     ~MotionTracker();
 };
 
+/**
+ * Kalman Filter for a group of objects
+ */
+class KalmanFilter {
+  public:
+    KalmanFilter();
+    ~KalmanFilter();
+    void update(CObjectList* classifierObjects);
+    CObjectList predict();
+  private:
+    CObjectList kalmanObjects;
+    CvBlobTrackPredictor* predictor1;
+    CvBlob blob1;
+};
