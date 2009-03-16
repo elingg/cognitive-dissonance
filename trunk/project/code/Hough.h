ce59db220dcf12d028d3087c8cb00b2b6a78e5ad
elingg <elingg@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-16 00:48:01 +0000

    forgot to add these

---
+++ trunk/project/code/Hough.h
@@ -0,0 +1,24 @@
+// Author: Cognitive-Dissonance team
+
+#pragma once
+#include <vector>
+#include "cv.h"
+
+using namespace std;
+
+class Hough {
+public:
+  Hough();
+  ~Hough();
+
+  void getCircles(vector<double>& feature_values,
+                        const IplImage* img) const;
+
+  void getLines(vector<double>& feature_values,
+                        const IplImage* img) const;
+
+
+private:
+
+};
+
