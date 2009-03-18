177cba834d4d2fc5d9c8e1a347346bc0431a7313
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-18 04:38:58 +0000

    adding ability to specify number of examples. look at options in final.sh

--- project/code/tune.cpp
+++ project/code/tune.cpp
@@ -40,6 +40,8 @@ void usage() {
     cerr << "    -depth         :: max tree depth " << endl;
     cerr << "    -trees         :: number of trees in boosted" << endl;
     cerr << "    -classifier    :: cv (OpenCV boost), ours (home grown adaboost)\n";
+    cerr << "    -examples      :: number of examples to use\n";
+    cerr << "    -trainerror    :: output training error as well (will slow it down)\n";
     cerr << "    -v             :: verbose" << endl;
     cerr << endl;
 }
@@ -53,11 +55,12 @@ int main(int argc, char *argv[])
     string classifier_name("cv");
     size_t nTrees=100;
     size_t nDepth=2;
+    size_t nExamples=0;
  
     // set defaults
     configurationFile = NULL;   // set using -c <filename>
     bVerbose = false;           // turn on with -v
-
+    bool bTrainError = false;
     // check arguments
     args = argv + 1;
     while (argc-- > 2) {
@@ -69,6 +72,9 @@ int main(int argc, char *argv[])
               usage(); 
               return -1;
             }
+        } else if (!strcmp(*args, "-examples")) {
+            argc--; args++;
+            nExamples = atoi(*args);
         } else if (!strcmp(*args, "-depth")) {
             argc--; args++;
             nDepth = atoi(*args);
@@ -93,6 +99,8 @@ int main(int argc, char *argv[])
         } else if (!strcmp(*args, "-h")) {
             usage();
             return 0;
+        } else if (!strcmp(*args, "-trainerror")) {
+            bTrainError = true;
         } else if (!strcmp(*args, "-v")) {
             bVerbose = !bVerbose;
         } else {
@@ -110,18 +118,19 @@ int main(int argc, char *argv[])
     cerr << "Using " << classifier_name << " for classification." << endl;
     cerr << "Using " << nTrees << " trees in the boosted tree." << endl;
     cerr << "Using max depth " << nDepth << " in the tree." << endl;
-    // cerr << "Writing trained classifier to " << configurationFile << endl;
-    cerr << "=================================================" << endl;
 
     // load the training file list
     TTrainingFileList fileList;
     fileList = getTrainingFiles(*args, ".jpg");
     // mix it up
     random_shuffle(fileList.files.begin(),fileList.files.end());
-     
-    size_t fold_size = fileList.files.size()/nFolds;
+    if(nExamples==0)
+      nExamples = fileList.files.size();
+    cerr << "Using " << nExamples << " examples" << endl;
+    cerr << "=================================================" << endl;
+    size_t fold_size = nExamples/nFolds;
     Timer t("entire experiment",true);
-    cout << "Running experiments on total of: " << fileList.files.size() << " files\n";
+    cout << "Running experiments on total of: " << nExamples << " files\n";
     Stats stats;
     cout << "Average test errors: \n";
     for(size_t ifold=0; ifold<nFolds; ++ifold) {
@@ -134,7 +143,7 @@ int main(int argc, char *argv[])
         size_t fold_start = fold_size*iofold;
         size_t fold_stop = fold_start+fold_size;
         if(iofold==(nFolds-1)) { // last fold
-          fold_stop = fileList.files.size();
+          fold_stop = nExamples;
         }
         for(size_t ifile=fold_start; ifile<fold_stop; ifile++) {
           if(ifold==iofold) {
@@ -145,14 +154,16 @@ int main(int argc, char *argv[])
         }
       } 
       // now train the classifier
-      showProgress(ifold*10+1,nFolds*10);
       if (!classifier.train(trainFileList)) {
         cerr << "ERROR: could not train classifier" << endl;
         exit(-1);
       }
-      showProgress(ifold*10+6,nFolds*10);
       double test_error = classifier.test(testFileList,stats);
-      showProgress(ifold*10+10,nFolds*10);
+      cout << "Test error: " << test_error << endl;
+      if(bTrainError) {
+        double train_error = classifier.test(trainFileList,stats);
+        cout << "Training error: " << train_error << endl;
+      }
       cout << "Fold " << ifold <<": " << test_error << endl;
     }
     stats.print_prediction_result();
