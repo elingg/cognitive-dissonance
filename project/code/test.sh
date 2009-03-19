a558157e64a47ec499f314ccdf4080b06afd37c6
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-19 17:02:48 +0000

    checking in with overlap_ratio back to the default of 0.05 so comparison numbers are meaningful. change if you need to play around.

--- project/code/test.sh
+++ project/code/test.sh
@@ -1,3 +1,3 @@
 #Milestone - shortcut script for running test
 #This is copied from pg. 7 of the handout
-./test -v -motiontracker -framestoskip 10 -f 500 -overlap_ratio 0.2 -c config.dat -g /afs/ir/class/cs221/vision/data/easy.xml -o test.xml /afs/ir/class/cs221/vision/data/easy.avi
+./test -v -motiontracker -framestoskip 10 -f 500 -overlap_ratio 0.05 -c config.dat -g /afs/ir/class/cs221/vision/data/easy.xml -o test.xml /afs/ir/class/cs221/vision/data/easy.avi
