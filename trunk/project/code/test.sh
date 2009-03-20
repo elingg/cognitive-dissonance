dfa864809e628759ed318cb30dd054eb027e7ea6
AlecMGo <AlecMGo@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-20 01:50:29 +0000

    Change default test.sh overlap ratio to 0.2

--- trunk/project/code/test.sh
+++ trunk/project/code/test.sh
@@ -1,3 +1,3 @@
 #Milestone - shortcut script for running test
 #This is copied from pg. 7 of the handout
-./test -motiontracker -framestoskip 10 -f 1000 -overlap_ratio 0.05 -c config.dat -g /afs/ir/class/cs221/vision/data/easy.xml -o test.xml /afs/ir/class/cs221/vision/data/easy.avi
+./test -motiontracker -framestoskip 10 -f 1000 -overlap_ratio 0.20 -c config.dat -g /afs/ir/class/cs221/vision/data/easy.xml -o test.xml /afs/ir/class/cs221/vision/data/easy.avi
