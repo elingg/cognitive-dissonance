82c8bdbdf11a36fb7202ab7891b8d0f939166100
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-20 05:47:18 +0000

    last options.

--- trunk/project/code/FinalSettings.h
+++ trunk/project/code/FinalSettings.h
@@ -3,12 +3,13 @@
 // Add final overriding settings here!!!!
 // Keep writing it in here, we will check with these uncommented....
 void overwriteWithFinalSettingsForSubmission(CommandOptions& args) {
-  // args.addUintOption("trees",1000);
-  // args.addUintOption("depth",2);
-  // args.addBoolOption("homegrownboost",false);
-  // args.addBoolOption("circle_feature", true);
-  // args.addBoolOption("corner_feature", true);
-  // args.addBoolOption("framest", true);
-  
+  args.addUintOption("motiontracker",true);
+  args.addUintOption("overlap_ratio",0.2);
+  args.addUintOption("trees",1000);
+  args.addUintOption("depth",2);
+  args.addBoolOption("homegrownboost",false);
+  args.addBoolOption("circle_feature", true);
+  args.addBoolOption("corner_feature", true);
+  args.addBoolOption("framestoskip", 5);
 }
 
