1fc8f12598a0921eca6868b7b3daad46e38f9cc1
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-20 06:02:36 +0000

    fixing.

--- project/code/FinalSettings.h
+++ project/code/FinalSettings.h
@@ -3,13 +3,13 @@
 // Add final overriding settings here!!!!
 // Keep writing it in here, we will check with these uncommented....
 void overwriteWithFinalSettingsForSubmission(CommandOptions& args) {
-  args.addUintOption("motiontracker",true);
-  args.addUintOption("overlap_ratio",0.2);
+  args.addBoolOption("motiontracker",true);
+  args.addDoubleOption("overlap_ratio",0.2);
   args.addUintOption("trees",1000);
   args.addUintOption("depth",2);
   args.addBoolOption("homegrownboost",false);
   args.addBoolOption("circle_feature", true);
   args.addBoolOption("corner_feature", true);
-  args.addBoolOption("framestoskip", 5);
+  args.addUintOption("framestoskip", 5);
 }
 
