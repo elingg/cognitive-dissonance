e11f9972b9d3442815a530db87b6ff4c27b64387
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-20 21:30:07 +0000

    undoing changes so we can now perform analysis.

--- trunk/project/code/FinalSettings.h
+++ trunk/project/code/FinalSettings.h
@@ -4,13 +4,13 @@
 // Add final overriding settings here!!!!
 // Keep writing it in here, we will check with these uncommented....
 void overwriteWithFinalSettingsForSubmission(CommandOptions& args) {
-  args.addBoolOption("motiontracker",true);
-  args.addDoubleOption("overlap_ratio",0.2);
-  args.addUintOption("trees",1000);
-  args.addUintOption("depth",2);
-  args.addBoolOption("homegrownboost",false);
-  args.addBoolOption("circle_feature", true);
-  args.addBoolOption("corner_feature", true);
-  args.addUintOption("framestoskip", 5);
+//  args.addBoolOption("motiontracker",true);
+//  args.addDoubleOption("overlap_ratio",0.2);
+//  args.addUintOption("trees",1000);
+//  args.addUintOption("depth",2);
+//  args.addBoolOption("homegrownboost",false);
+//  args.addBoolOption("circle_feature", true);
+//  args.addBoolOption("corner_feature", true);
+//  args.addUintOption("framestoskip", 5);
 }
 
