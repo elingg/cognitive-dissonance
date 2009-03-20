04e8be562e0a02396cbb8221cabb26356a4a5afe
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-20 05:49:52 +0000

    train olptions.

--- project/code/train.sh
+++ project/code/train.sh
@@ -1,3 +1,1 @@
-#Milestone - shortcut for running train
 ./train -c config.dat /afs/ir/class/cs221/vision/data/vision_all
-#./train -c config.dat /afs/ir.stanford.edu/users/m/a/manand/public/data_small
