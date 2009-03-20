5a68faa865b9d8ff795834f6515e598fe1d7642b
madhavan.anand <madhavan.anand@257f2f30-e9bf-11dd-b00f-9fabb4b2ff13>
2009-03-20 05:37:26 +0000

    comemts for final checkin almost.

--- project/code/FinalSettings.h
+++ project/code/FinalSettings.h
@@ -3,9 +3,12 @@
 // Add final overriding settings here!!!!
 // Keep writing it in here, we will check with these uncommented....
 void overwriteWithFinalSettingsForSubmission(CommandOptions& args) {
-  // args.addOption("trees",1000);
-  // args.addOption("depth",2);
-  // args.addOption("homegrownboost",false);
+  // args.addUintOption("trees",1000);
+  // args.addUintOption("depth",2);
+  // args.addBoolOption("homegrownboost",false);
+  // args.addBoolOption("circle_feature", true);
+  // args.addBoolOption("corner_feature", true);
+  // args.addBoolOption("framest", true);
   
 }
 
